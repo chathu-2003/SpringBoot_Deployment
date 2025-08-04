7d9ae2bc3d61cb4509000b2422bbbb926cda30a4
chathu_2003 <chathuralakshan0507@gmail.com>
2025-08-04 12:00:58 +0530

    update Api Response

---
+++ src/main/java/lk/ijse/gdse72/springboot_deployment/dto/APIResponse.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse72.springboot_deployment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class APIResponse<T> {
+    private int status;
+    private String message;
+    private T data;
+
+}
