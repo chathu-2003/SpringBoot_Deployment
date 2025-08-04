80b0288025a41863fadc5cd1366752ad6d7ab88d
chathu_2003 <chathuralakshan0507@gmail.com>
2025-08-04 10:13:29 +0530

    Initial commit

---
+++ src/test/java/lk/ijse/gdse72/springboot_deployment/SpringBootDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse72.springboot_deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
